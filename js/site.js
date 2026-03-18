d0860de69689e87af9753bc00368b559ea57a168
MOYO2702 <olayemisimoyo30@gmail.com>
2026-03-19 02:54:25 +0400

    Replace placeholder favicon with logo mark

--- js/site.js
+++ js/site.js
@@ -24,11 +24,12 @@ function ensureSharedFavicons() {
   existingIcons.forEach((icon) => icon.remove());
 
   const icons = [
-    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-128x128.png", sizes: "128x128" },
-    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-256x256.png", sizes: "256x256" },
-    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-64x64.png", sizes: "64x64" },
-    { rel: "apple-touch-icon", href: "/images/favicon/favicon-256x256.png", sizes: "256x256" },
-    { rel: "shortcut icon", href: "/images/favicon/favicon-128x128.png" }
+    { rel: "icon", type: "image/svg+xml", href: "/images/favicon/favicon.svg?v=4", sizes: "any" },
+    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-64x64.png?v=4", sizes: "64x64" },
+    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-128x128.png?v=4", sizes: "128x128" },
+    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-256x256.png?v=4", sizes: "256x256" },
+    { rel: "apple-touch-icon", href: "/images/favicon/favicon-256x256.png?v=4", sizes: "256x256" },
+    { rel: "shortcut icon", href: "/favicon.ico?v=4" }
   ];
 
   icons.forEach(({ rel, type, href, sizes }) => {
