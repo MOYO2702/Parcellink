6928eb3171284b50b7f67d45b83048af4ce8e3cf
MOYO2702 <olayemisimoyo30@gmail.com>
2026-03-19 03:05:58 +0400

    Use bold filled favicon for stronger tab presence

--- js/site.js
+++ js/site.js
@@ -24,12 +24,9 @@ function ensureSharedFavicons() {
   existingIcons.forEach((icon) => icon.remove());
 
   const icons = [
-    { rel: "icon", type: "image/svg+xml", href: "/images/favicon/favicon.svg?v=6", sizes: "any" },
-    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-64x64.png?v=6", sizes: "64x64" },
-    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-128x128.png?v=6", sizes: "128x128" },
-    { rel: "icon", type: "image/png", href: "/images/favicon/favicon-256x256.png?v=6", sizes: "256x256" },
-    { rel: "apple-touch-icon", href: "/images/favicon/favicon-256x256.png?v=6", sizes: "256x256" },
-    { rel: "shortcut icon", href: "/favicon.ico?v=6" }
+    { rel: "icon", type: "image/svg+xml", href: "/images/favicon/favicon.svg?v=7", sizes: "any" },
+    { rel: "apple-touch-icon", href: "/images/favicon/favicon.svg?v=7", sizes: "180x180" },
+    { rel: "shortcut icon", href: "/images/favicon/favicon.svg?v=7" }
   ];
 
   icons.forEach(({ rel, type, href, sizes }) => {
